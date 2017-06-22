b9d371de3c5de8b4db83a240c807e1ddc6087e5e
Snigdha Sur <sur.snigdha@gmail.com>
2017-06-22 19:06:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+
+    value_all = name_hash.collect do |key, value|
+      value
+    end
+    order = [value_all[0]]
+    i = 0
+    while i < value_all.length
+      if value_all[i] < order[0]
+        order.unshift(value_all[i])
+      end
+      i += 1
+    end
+    name_hash.index(order[0])
+
 end
